25044ce1aeaf4a304bef16038e11ec1b84f3e3d7
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-01-18 18:29:17 +0200

    Add class figure

---
+++ ConsoleApplication1/Figura.cpp
@@ -0,0 +1,13 @@
+#include "Figura.h"
+
+Figura::Figura()
+{
+	this->x = 0;
+	this->y = 0;
+}
+
+Figura::Figura(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
---
+++ ConsoleApplication1/Figura.h
@@ -0,0 +1,18 @@
+#pragma once
+class Figura
+{
+protected:
+	int x;
+	int y;
+public:
+	Figura();
+	Figura(int x, int y);
+
+	int getX() const {
+		return this->x;
+	}
+	int getY() const {
+		return this->y;
+	}
+};
+
